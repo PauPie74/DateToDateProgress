ac859052dfb28f32d0f8cc1585dedb7f3a950171
Paulina <79440830+PauPie74@users.noreply.github.com>
2022-01-07 20:00:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ function dateToPercent(days, totalDays) {
 
 function Weeks(days) {
   var w = days/7
-  return w.toFixed(0) + " weeks " + days%7 + " days"
+  return w.toFixed(0) + " weeks " + days%7 + " days";
 }
 
-var toGoDays = Days(dateToDate(semesterEnd, now))
+var toGoDays = Days(dateToDate(semesterEnd, now));
 
-var passedDays = Days(dateToDate(now, semesterStart))
+var passedDays = Days(dateToDate(now, semesterStart));
 
-var TotalDays = Days(dateToDate(semesterEnd, semesterStart))
+var TotalDays = Days(dateToDate(semesterEnd, semesterStart));
 
-document.getElementById("toGo").innerHTML = toGoDays + " days";
+document.getElementById("toGo").innerHTML = toGoDays + " days (" + Weeks(toGoDays) + ")";
 
 document.getElementById("academicYearProgress").value = dateToPercent(passedDays, TotalDays);
 
@@ -42,22 +42,22 @@ function sem(date){
   }
 }
 
-var semester = sem(now)
+var semester = sem(now);
 
 document.getElementById("Semester").innerHTML = semester;
 
 if (semester == "Summer 2021/2022"){
-      var totalSummer = Days(dateToDate(semesterEnd, summer))
-    var toGoDaysSummer = Days(dateToDate(now, summer))
-  var passedDaysSummer = Days(dateToDate(now, summer))
-  document.getElementById("semesterProgress").value = dateToPercent(passedDaysSummer, totalSummer);
-  document.getElementById("semesterPercent").innerHTML = dateToPercent(passedDaysSummer, totalSummer).toFixed(2) + "%";
+    var totalSummer = Days(dateToDate(semesterEnd, summer));
+    var toGoDaysSummer = Days(dateToDate(now, summer));
+    var passedDaysSummer = Days(dateToDate(now, summer));
+    document.getElementById("semesterProgress").value = dateToPercent(passedDaysSummer, totalSummer);
+    document.getElementById("semesterPercent").innerHTML = dateToPercent(passedDaysSummer, totalSummer).toFixed(2) + "%";
   } else if (semester == "Winter 2021/2022") {
-    var totalWinter = Days(dateToDate( summer, semesterStart))
-    var toGoDaysWinter = Days(dateToDate(summer, now))
-  var passedDaysWinter = Days(dateToDate(now, semesterStart))
-  document.getElementById("semesterProgress").value = dateToPercent(passedDaysWinter, totalWinter);
-  document.getElementById("semesterPercent").innerHTML = dateToPercent(passedDaysWinter, totalWinter).toFixed(2) + "%";
+    var totalWinter = Days(dateToDate( summer, semesterStart));
+    var toGoDaysWinter = Days(dateToDate(summer, now));
+    var passedDaysWinter = Days(dateToDate(now, semesterStart));
+    document.getElementById("semesterProgress").value = dateToPercent(passedDaysWinter, totalWinter);
+    document.getElementById("semesterPercent").innerHTML = dateToPercent(passedDaysWinter, totalWinter).toFixed(2) + "%";
   }
 
 
